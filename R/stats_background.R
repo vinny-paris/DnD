c677e0370138a81487feded9471a6dd8433f09fb
Vincent Paris <vincentparis@anarchistoparis.student.iastate.edu>
2017-04-06 14:46:38 -0400

    Clean up

--- R/stats_background.R
+++ R/stats_background.R
@@ -1,9 +1,7 @@
-
 DnD <- function(which.person = which.person, which.level = which.level, ...){
   
 background <- function() {
-
-   system("../Downloads/phantomjs scrape_techstars.js")
+  system("../Downloads/phantomjs scrape_techstars.js")
 html <- read_html("techstars.html")
 Holding <- html %>% html_nodes("dl, dd") %>% html_text %>% .[2] %>% capitalize
 return(Holding)}
@@ -42,35 +40,6 @@ which.personality <- unlist(str_split(back, " "))[1]
 }
 
 
-
-background <- function() {
-  system("./phantomjs scrape_techstars.js")
-html <- read_html("techstars.html")
-Holding <- html %>% html_nodes("dl, dd") %>% html_text %>% .[2] %>% capitalize
-return(Holding)}
-
-Base.Method <- function(Stats= c("str", "con", "dex", "int", "wis", "char")){
-  Holding <- NULL
-  for (i in 1:length(Stats))
-    {
-  j <- runif(3, min = 0, max = 6)
-  Holding[i] <- sum(ceiling(j))
-  }
-    
-  Finale <- Holding[sort.list(Holding, decreasing = TRUE)]
-  names(Finale) <- Stats
-  mod <- floor((Finale - 10)/2)
-  Finale <- cbind(Finale, mod)
-  colnames(Finale) <- c("Stats", "mod")
-    return(Finale)
 }
 
 
-
-
-my.function <- funtion(url) {
-        s <- read_html(paste(url))
-        html %>% html("#info .poptip") %>% html_text()
-        table <- html %>% html_table()
-        
-        
