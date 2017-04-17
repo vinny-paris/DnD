64077af9e7b37fa1829945185ac3d67a1c58bda6
Vincent Paris <vincentparis@Vincents-MacBook-Air.local>
2017-04-16 20:22:36 -0400

    Documentation for Class_SPells and character_tables

---
+++ R/Class_Spells.R
@@ -0,0 +1,7 @@
+#' Spells Known by Class fully labeld
+#'
+#' This is a list made up of the 8 spell casting classes. We do not preserve spell level but we do preserve the full name. 
+
+#' @source {Dungeon and Dragons Fifth Edition Player's Hand Book}
+
+"Class_Spells"
