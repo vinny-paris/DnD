19b8ecbd99478ec40a2e79fcbc8c6795e06a28f9
Vincent Paris <vincentparis@Vincents-MacBook-Air.local>
2017-04-17 21:43:56 -0400

    Documentation added

--- R/stats_background.R
+++ R/stats_background.R
@@ -1,3 +1,36 @@
+#' DnD Character Generator
+#' 
+#' Creates a DnD 5th edition character that is roughly built according to the rules
+#' @param method The method used to roll the dice. Options include Commoner.Method (which is defaulted inside of DnD), Base.Method, Pilsfer.Method, Heroic.Method, and Ferguson.Method. 
+#' @seealso Method and Stats are covered more in depth at \code{\link{stat.roll}}
+#' @seealso Method for obtaining the spells for a character at \code{\link{my.spells}}
+#' @export
+#' @param which.person Who is this character? Any character string will work, don't need to use ""
+#' @param which.level What level is the character? Please only choose numbers between 1 and 20.
+#' \item{Gamer}{which.person}
+#' \item{Race}{The race of the character, one of 9 possible}
+#' \item{Level}{which.level}
+#' \item{Class}{The class of the character, one of 12 possible}
+#' \item{Proficiency.Bonus}{The proficiency bonus modifier for known skills and saves}
+#' \item{Saves}{Known saves of the class, 1 for known, 0 for unkown}
+#' \item{Skills}{Known skills of the class}
+#' \item{hp}{The maximum and current hit points of a character}
+#' \item{Statistics}{The Stats and mod created for the character. Go to stat.roll help page for more indepth coverage. IMPORTANT: Stats are assigned according to the class inside the DnD function so do not try to pass this parameter in!}
+#' \item{Weapons}{THree randomly assigned weapons regardless of proficiency or class}
+#' \item{Armor.Class}{What armor is the character wearing and what is his AC?}
+#' \item{Background}{Personality and motivation for the character}
+#' \item{Spells}{The spells known/prepared for the character. See the help page for my.spells for more}
+#' 
+#' @examples
+#' DnD(Blacksmith, 5)
+#' DnD(Town.Clerk, 2, method = Base.Method)
+
+
+
+
+
+
+
 DnD <- function(which.person = which.person, which.level = which.level, ...){
 
     
@@ -55,6 +88,7 @@ which.personality <- unlist(str_split(back, " "))[1]
                 hp = hp.calculator(paste(which.class), which.level) + which.level*stats[2, "con"],
                 Statistics = stats,
                 Weapons = Weapons[ceiling(runif(3, min = 0, max = 37))],
+                Armor.Class = armor.class(which.class, stats),
                 Background = back,
                 Spells = Spells
        )
