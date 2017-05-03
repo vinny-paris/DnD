321765051c87fa8ebcfbea99ae6a347cdbeaec89
vinny-paris <vinny@iastate.edu>
2017-05-03 17:10:38 -0500

    documentation issue

--- R/saving_throw.R
+++ R/saving_throw.R
@@ -26,9 +26,6 @@ saving_throw <- function(which.character, save, ...){
 #' @param ... Additional arguments to be passed to the roll() function.
 #' @return Will return the the saving throw including the proficiency bonus and character statistic modifier added properly.
 #' @note There is no way to tell if the dice rolled used adv/dis after the fact so be careful in specifing the parameters.
-#' @examples
-#' Blacksmith_Jamestown <- DnD(Josh, 4)
-#' saving_throw(Blacksmith_Jamestown, str)
 
 
 saving_throw.DnD <- function(which.character, save,...){
@@ -52,8 +49,6 @@ saving_throw.DnD <- function(which.character, save,...){
 #' @param ... Additional arguments to be passed to the roll() function.
 #' @return Will return the the saving throw including the proficiency bonus and character statistic modifier added properly.
 #' @note There is no way to tell if the dice rolled used adv/dis after the fact so be careful in specifing the parameters.
-#' @example
-#' saving_throw("apple", str)
 
 saving_throw.default <- function(which.character,save,...)
         {
