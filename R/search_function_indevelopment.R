2fcae286dc1830b670249fcb9f5afb1ab8137b88
vinny-paris <vinny@iastate.edu>
2017-05-19 12:06:52 -0500

    can look up names using partial character strings, quick load if there is a single direct match (heal vs mass heal is a no go), option to return names only (default) vs entire list text

--- R/search_function_indevelopment.R
+++ R/search_function_indevelopment.R
@@ -1,9 +1,21 @@
 
-testing <- spell_description %>% map(names) %>% map(grep, pattern = "skin")
+search_spells <- function(pattern, names.only = T, quick.return = T){
+
+  
+pattern <- deparse(substitute(pattern))  
+
+  
+testing <- spell_description %>% map(names) %>% map(grep, pattern = pattern)
 testing
 
+f <- ifelse(length(unlist(testing)) == 0, paste("No Matches. Sorry but check spelling maybe?  :/"), {
 
-nam <- row.names(melt(unlist(testing)))
+names.only <- ifelse(sum(pattern == unlist(Spell_List)) == 1 & length(unlist(testing)) == 1 & quick.return == T, F, names.only)
+
+
+
+
+nam <- melt(testing[lapply(testing,length)>0])[,2]
 nam
 
 
@@ -15,7 +27,23 @@ hope <- cbind(unit.num, nam)
 holding <- NULL
 for(i in 1:dim(hope)[1]){
 lis <- list( spell_description[[hope[i,2]]][[as.numeric(hope[i,1])]])
+notice <- capitalize(gsub('\\.', ' ', hope[i,2]))
+notice <- ifelse(notice == 'Cantrip', 'Level 0', notice)
+names(lis) <- list(Spell_List[[notice]][[as.numeric(hope[i,1])]])
 holding <- c(holding, lis)
-  }
+}
+spell_nams <- (names(holding))
+findings <- if(names.only == T){spell_nams} else{holding}
+return(findings)
 
+})
+return(f)
+}
 
+search_spells(skin)
+search_spells(fire)
+search_spells(Heal, names.only = T)
+search_spells(Stoneskin)
+search_spells(skin, names.only = F)
+search_spells(Stoneskin, quick.return = F)
+search_spells(fare)
