666704be23b452118bb02546299ff61d148c7df9
vinny-paris <vinny@iastate.edu>
2017-05-19 11:25:36 -0400

    Starting to actually work kind of maybe. Names needed, fuzzy names, list of names vs output (function parameter???) and then made into function

--- R/search_function_indevelopment.R
+++ R/search_function_indevelopment.R
@@ -1,17 +1,21 @@
 
-jack <- spell_description %>% map(names) %>% map(grep, pattern = "fire")
-jack
+testing <- spell_description %>% map(names) %>% map(grep, pattern = "skin")
+testing
 
 
-nam <- names(unlist(jack))
+nam <- row.names(melt(unlist(testing)))
 nam
 
 
-unit.num <- unlist(jack)
+unit.num <- unlist(testing)
 unit.num
 
+hope <- cbind(unit.num, nam)
 
-
-list(nam, spell_description[[nam]][[unit.num]])
+holding <- NULL
+for(i in 1:dim(hope)[1]){
+lis <- list( spell_description[[hope[i,2]]][[as.numeric(hope[i,1])]])
+holding <- c(holding, lis)
+  }
 
 
