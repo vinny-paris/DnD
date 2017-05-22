e66f295df6ee9eff9f146b4e4b8b953bc58baa17
vinny-paris <vinny@iastate.edu>
2017-05-22 17:05:05 -0500

    Documented

--- inst/para_search.R
+++ inst/para_search.R
@@ -1,3 +1,12 @@
+#' Finds the pattern matches in paragraphs of spells
+#' 
+#' We searched the text of the spells for a matching character string and returned the names of the spells they were in.
+#' @param pattern The pattern to be searched for
+#' @return Names of spells with paragraph matches
+
+
+
+
 para_search <- function(pattern){
 
 testing <- at_depth(spell_description, 2, grep, pattern = pattern)
